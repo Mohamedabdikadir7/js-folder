99e95062e7137250dc384f178b8bdfffc5a24ae0
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-05-18 21:43:10 +0300

    my new js repo

---
+++ string.js
@@ -0,0 +1,19 @@
+let string = 'mohamed'
+// let game = 'soccer'
+// let age = 45
+// let course = 'DIT'
+
+// let message = `my name is ${string} am ${age} and i i love ${game}` ;
+
+// console.log(message);
+
+
+
+
+
+
+
+
+let email = 'mohamed'
+let z = email.toUpperCase()
+console.log(z)
