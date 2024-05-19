4055d2a2675312029aacc434ca4458cd6f67e826
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-05-19 22:38:56 +0300

    js function

---
+++ function.js
@@ -0,0 +1,16 @@
+//  Function to Add Two Numbers
+function add(a, b) {
+    return a + b;
+}
+
+// Example usage:
+console.log(add(3, 4)); // Output: 7
+
+// Function to Check if a Number is Even
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+// Example usage:
+console.log(isEven(4)); // Output: true
+console.log(isEven(7)); // Output: false
