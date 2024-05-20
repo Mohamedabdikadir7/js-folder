38f3e69aa995452d9a23da93c5307d106ff6fe11
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-05-20 23:36:47 +0300

    learning js loop

--- loop.js
+++ loop.js
@@ -1,3 +1,5 @@
+
+// Loops in JavaScript are a fundamental concept used to execute a block of code multiple times
 ///  Loop to Print Numbers from 1 to 5
 for (let i = 1; i <= 5; i++) {
     console.log(i);
@@ -9,3 +11,12 @@ for (let i = 1; i <= 5; i++) {
 // 3
 // 4
 // 5
+
+// 1. for loop
+// syntax
+for (initialization; condition; increment) {
+    // code block to be executed
+}
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
