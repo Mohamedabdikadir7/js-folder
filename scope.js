f2f546c614a7c68efe7f072ce561368aa89af7e9
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-05-22 21:48:34 +0300

    javascript scope

---
+++ scope.js
@@ -0,0 +1,40 @@
+/// they are three types of scopes
+/// Global Scope
+// Function Scope
+// Block Scope (introduced in ES6)
+
+// GLOBAL SCOPE
+// Variables declared outside of any function or block have global scope. They can be accessed from anywhere within the script, including inside functions and blocks.
+
+// example 1
+
+// Global scope variable
+const globalVar = 'I am in global scope';
+
+function foo() {
+    console.log(globalVar); // Accessing globalVar from inside the function
+}
+
+foo(); // Output: I am in global scope
+
+// In this example, globalVar is accessible both inside and outside the foo function because it's declared in the global scope.
+
+// Function Scope
+
+/// Variables declared inside a function have function scope. They are accessible only within the function in which they are defined.
+
+function outerFunction() {
+    const localVar = 'I am in function scope';
+
+    function innerFunction() {
+        console.log(localVar); // Accessing localVar from inside the inner function
+    }
+
+    innerFunction();
+}
+
+
+outerFunction(); // Output: I am in function  n scope
+
+
+
