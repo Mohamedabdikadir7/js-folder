70e1ef9b4a05e148a1abcb6931cff95c0be4970c
mohamedabdikadir7 <mohaabdikadiir@gmail.com>
2024-05-23 21:04:32 +0300

    DOM evetlistener

---
+++ eventlistener.js
@@ -0,0 +1,10 @@
+/// anything that is gonna happen is an event
+const button = document.querySelector('button');
+button.addEventListener('click', function() {
+    alert('Button clicbked!');
+});
+
+document.getElementById('newbutton').addEventListener("click", function () {
+    alert('we love you')
+})
+
